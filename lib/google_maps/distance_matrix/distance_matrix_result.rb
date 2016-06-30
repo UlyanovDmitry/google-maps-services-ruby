70390441e44d86a70e9e565d24b34495595dee6e
demon <demon@pglu.pro>
2016-06-30 11:56:11 +0300

    Check @status == 'OK'

--- lib/google_maps/distance_matrix/distance_matrix_result.rb
+++ lib/google_maps/distance_matrix/distance_matrix_result.rb
@@ -8,9 +8,15 @@ module GoogleMaps
       raise ArgumentError, 'options is not class Hash' unless options.class.to_s == 'Hash'
       @origins = options['origins'].to_s
       @destinations = options['destinations'].to_s
-      @distance = Distance.new options['distance']
-      @duration = Duration.new options['duration']
+      puts "#{options['distance']}"
       @status = options['status'].to_s
+      if @status == 'OK'
+        @distance = Distance.new options['distance']
+        @duration = Duration.new options['duration']
+      else
+        @distance = Distance.new text: '', value: ''
+        @duration = Duration.new text: '', value: ''
+      end
     end
 
     def to_s
