6dfa876767f02480e63b04348978e3e1e96f8a67
ulyanovda <ulyanovda8@gmail.com>
2018-07-23 00:48:50 +0300

    Add rspec

--- google-maps-services.gemspec
+++ google-maps-services.gemspec
@@ -19,4 +19,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = %w(lib)
 
   spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 end
---
+++ spec/vcr_helper.rb
@@ -0,0 +1,20 @@
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.ignore_localhost = true
+  c.configure_rspec_metadata!
+
+  c.before_record do |i|
+    i.response.body.force_encoding('UTF-8')
+  end
+end
+
+DISTANCE_MATRIX_VCR = {
+    cassette_name: 'distance_matrix',
+    record: :once,
+    allow_playback_repeats: true,
+    erb: true,
+    match_requests_on: [:method, :uri, :body]
+}
